2ea7dcf46f0ce6b3adae32c9565560d4a3242295
bigfatmoon <a21mor@mail.ru>
2020-03-25 00:48:35 +0300

    minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,23 +6,24 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        StringBuilder res = new StringBuilder();
-        Files.readAllLines(Path.of("src/main/resources/test.txt")).forEach(line -> {
-            String[] mas = line.split("\\|");
-            String[] name = Pattern.compile("[А-Я][а-я]+").matcher(mas[0]).results().limit(2).map(MatchResult::group).toArray(String[]::new);
-            res.append(name.length == 2 ? name[0] + " " + name[1] + "|" : "|");
-            Pattern.compile("\\d{1,3}").matcher(mas[1]).results().limit(1).forEach(m -> res.append(m.group()));
-            res.append("|");
-            StringBuilder phone = new StringBuilder("+");
-            Pattern.compile("\\d+").matcher(mas[2]).results().forEach(m -> phone.append(m.group()));
-            if (phone.length() == 12)
-                res.append(phone.insert(2, "(").insert(6, ")").insert(10, "-").insert(13, "-"));
-            res.append("|");
-            mas[3] = mas[3].replaceFirst("@+", "@").replaceFirst("\\.+", ".");
-            if (mas[3].matches("\\w+@\\w+\\.[a-z]+"))
-                res.append(mas[3]);
-            res.append("\n");
-        });
-        Files.writeString(Path.of("src/main/resources/res.txt"), res);
+        System.out.println("Banana");
+//        StringBuilder res = new StringBuilder();
+//        Files.readAllLines(Path.of("src/main/resources/test.txt")).forEach(line -> {
+//            String[] mas = line.split("\\|");
+//            String[] name = Pattern.compile("[А-Я][а-я]+").matcher(mas[0]).results().limit(2).map(MatchResult::group).toArray(String[]::new);
+//            res.append(name.length == 2 ? name[0] + " " + name[1] + "|" : "|");
+//            Pattern.compile("\\d{1,3}").matcher(mas[1]).results().limit(1).forEach(m -> res.append(m.group()));
+//            res.append("|");
+//            StringBuilder phone = new StringBuilder("+");
+//            Pattern.compile("\\d+").matcher(mas[2]).results().forEach(m -> phone.append(m.group()));
+//            if (phone.length() == 12)
+//                res.append(phone.insert(2, "(").insert(6, ")").insert(10, "-").insert(13, "-"));
+//            res.append("|");
+//            mas[3] = mas[3].replaceFirst("@+", "@").replaceFirst("\\.+", ".");
+//            if (mas[3].matches("\\w+@\\w+\\.[a-z]+"))
+//                res.append(mas[3]);
+//            res.append("\n");
+//        });
+//        Files.writeString(Path.of("src/main/resources/res.txt"), res);
     }
 }
